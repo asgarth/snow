ba0df7009a8152023fcd5f2d36dd072de54b4f9f
Sergio <asgarth@gmail.com>
2012-08-07 16:07:03 +0200

    added some platform utility methods

--- src/org/snow/util/Constants.java
+++ src/org/snow/util/Constants.java
@@ -5,11 +5,6 @@ public class Constants {
 	/** Utility class, prevent instantiation. */
 	private Constants() { }
 
-	/** common path */
-	public static final String HOME = System.getProperty( "user.home" );
-
-	public static final String DESKTOP = System.getProperty( "user.home" ) + "/Desktop";
-
 	/** common resources */
 	public static final String CURSOR_IMAGE_HAND = "/images/hand.png";
 	public static final String FOLDER_ICON_IMAGE = "/images/folder.png";
---
+++ src/org/snow/util/Platform.java
@@ -0,0 +1,78 @@
+package org.snow.util;
+
+/** Provide simplified platform information. */
+public class Platform {
+    public static final int UNSPECIFIED = -1;
+    public static final int MAC = 0;
+    public static final int LINUX = 1;
+    public static final int WINDOWS = 2;
+
+    private static final int osType;
+
+    static {
+        String osName = System.getProperty("os.name");
+        
+        if (osName.startsWith("Linux")) {
+            osType = LINUX;
+        }
+        else if (osName.startsWith("Mac") || osName.startsWith("Darwin")) {
+            osType = MAC;
+        }
+        else if (osName.startsWith("Windows")) {
+            osType = WINDOWS;
+        }
+        else {
+            osType = UNSPECIFIED;
+        }
+    }
+    
+    /** Utility class, prevent instantiation. */
+    private Platform() { }
+    
+    public static int getOSType() {
+        return osType;
+    }
+    
+    public static boolean isMac() {
+        return osType == MAC;
+    }
+    
+    public static boolean isLinux() {
+        return osType == LINUX;
+    }
+    
+    public static final boolean isWindows() {
+        return osType == WINDOWS;
+    }
+    
+    public static String getUserHome() {
+    	if (isWindows()) {
+    		final String userProfile = System.getenv("USERPROFILE");
+    		if (userProfile != null && ! userProfile.isEmpty())
+    			return userProfile;
+
+    		return System.getenv("HOMEDRIVE") + System.getenv("HOMEPATH");
+    	}
+    	else {
+    		return System.getProperty("user.home");
+    	}
+    }
+    
+    public static String getPathSeparator() {
+		return System.getProperty("file.separator");
+	}
+    
+    public static String getUserApp(final String appName) {
+    	if (isWindows()) {
+    		final String addData = System.getenv("APPDATA");
+    		if (addData != null && ! addData.isEmpty())
+    			return addData + getPathSeparator() + appName;
+    		
+    		return getUserHome() + getPathSeparator() + appName;
+    	}
+    	else {
+    		return getUserHome() + getPathSeparator() + "." + appName;
+    	}
+    }
+
+}
